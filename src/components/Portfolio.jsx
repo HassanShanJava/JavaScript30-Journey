4de6e6dd2bf1a89ed1fd72b77faa8a760e00a834
HassanShanJava <hassanshan675@gmail.com>
2022-11-01 03:21:25 +0500

    fixed responsiveness

--- src/components/Portfolio.jsx
+++ src/components/Portfolio.jsx
@@ -14,16 +14,16 @@ const Portfolio = () => {
       className="w-full pt-8 md:h-full text-gray-300 bg-gradient-to-t from-amber-200 via-violet-600 to-sky-900"
     >
       {/* container */}
-      <div className="max-w-[1000px] mx-auto p-4 flex flex-col flex-wrap justify-center  text-center w-full h-full">
+      <div className="max-w-[1000px] mx-auto p-4 flex flex-col justify-center  text-center w-full h-full">
         <div className="pb-8 ">
-          <p className="text-4xl  font-bold text-gray-300 inline border-b-4 border-pink-600">
+          <p className="text-4xl font-bold text-gray-300 inline border-b-4 border-pink-600">
             JavaScript30 days Challenge
           </p>
           <p className="text-left py-6 ">{string2}</p>
         </div>
 
         {/* container for projects */}
-        <div className="grid sm:grid-cols-3 md:grid-cols-4 justify-center gap-4">
+        <div className="grid sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 items-center gap-4">
 
 
 
